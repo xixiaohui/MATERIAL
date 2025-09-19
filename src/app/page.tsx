7a5edfd0eb3d30e571cedc1de0caa116713dd4a8
unknown <yamazake@yeah.net>
2025-09-19 15:41:42 +0800

    gpx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="/cycle"
           target="_blank"
           rel="noopener noreferrer"
         >
