667a8d8dcb7906d551fe1c4298b8cd7550a75443
unknown <yamazake@yeah.net>
2025-10-10 14:23:51 +0800

    add prisma

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,25 +1,36 @@
-import { prisma } from "@/lib/prisma";
+'use client'
+
+import { useEffect, useState } from "react";
 
 import { Drawing } from "@/generated/prisma";
 
 import Link from "next/link";
 
+export default function GalleryPage() {
+  const [drawings, setDrawings] = useState<Drawing[]>([]);
 
-export default async function GalleryPage() {
-    
-    const drawings: Drawing[] = await prisma.drawing.findMany({orderBy:{ createdAt:'desc'}});
+  useEffect(() => {
+    fetch("/api/drawings")
+      .then((res) => res.json())
+      .then(setDrawings);
+  }, []);
 
-    return(
-        <main className="p-8 grid grid-cols-2 md:grid-cols-4 gap-4">
-            {drawings.map(d =>(
-                <Link key={d.id} href={`/d/${d.id}`} className="border p-2 rounded hover:shadow-md">
+  return (
+    <main className="p-8 grid grid-cols-2 md:grid-cols-4 gap-4">
+      {drawings.map((d) => (
+        <Link
+          key={d.id}
+          href={`/d/${d.id}`}
+          className="border p-2 rounded hover:shadow-md"
+        >
+          <img
+            src={d.thumbnail || "/yumrin.svg"}
+            className="aspect-video object-cover"
+          />
 
-                    <img src={d.thumbnail || '/yumrin.svg'} className="aspect-video object-cover" />
-
-                    <h3 className="text-sm mt-2">{d.title}</h3>
-                </Link>
-            ))}
-
-        </main>
-    );
+          <h3 className="text-sm mt-2">{d.title}</h3>
+        </Link>
+      ))}
+    </main>
+  );
 }
