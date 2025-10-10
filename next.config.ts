6d4e62abb12ef61d4e923c49a76221b5fa6a1667
unknown <yamazake@yeah.net>
2025-10-10 13:43:56 +0800

    add prisma

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,33 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-
-  
-};
-
-export default nextConfig;
-
-
-// import type { NextConfig } from 'next';
+// import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
-//   // ✅ 仅显示构建错误，不显示警告
-//   webpack: (config, { isServer }) => {
-//     config.stats = {
-//       all: false,
-//       errors: true, // 仅显示错误
-//       errorDetails: true, // 显示错误细节
-//     };
-//     return config;
-//   },
+//   /* config options here */
 
-//   // ✅ 构建时忽略 ESLint 警告
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-
-//   // ✅ 构建时忽略 TypeScript 类型错误（可选）
-//   // ⚠️ 注意：这会忽略真正的 TS 报错，请谨慎使用
-//   // typescript: {
-//   //   ignoreBuildErrors: true,
-//   // },
+  
 // };
 
 // export default nextConfig;
+
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  // ✅ 仅显示构建错误，不显示警告
+  webpack: (config, { isServer }) => {
+    config.stats = {
+      all: false,
+      errors: true, // 仅显示错误
+      errorDetails: true, // 显示错误细节
+    };
+    return config;
+  },
+
+  // ✅ 构建时忽略 ESLint 警告
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+
+};
+
+export default nextConfig;
